40b82eaca30662935a6d2a7f19359c1c140b7339
unknown <z00327906@S00304949P32124.china.huawei.com>
2015-11-02 10:08:18 +0800

    optimize

--- deployment_scripts/puppet/manifests/neutron.pp
+++ deployment_scripts/puppet/manifests/neutron.pp
@@ -5,20 +5,18 @@ neutron_plugin_ml2 {
   'ml2_onos/password':           value => 'admin';
   'ml2_onos/username':           value => 'admin';
   'ml2_onos/url_path':           value => "http://$::ipaddress_br_mgmt:8181/onos/vtn";
-}
-
+}->
 exec{ 'Configure Neutron3':
         command  => "mysql -e 'drop database if exists neutron;';
 		    mysql -e 'create database neutron character set utf8;';
 		    mysql -e \"grant all on neutron.* to 'neutron'@'%';\";
 		    neutron-db-manage --config-file /etc/neutron/neutron.conf --config-file /etc/neutron/plugin.ini upgrade head;",
 } ->
-
-        exec{ 'restart neutron':
+exec{ 'restart neutron':
         command  => "service neutron-server restart",
 } ->
 service {'neutron-dhcp-agent':
- 	ensure => running,
+		ensure => running,
         enable => true,	
 } ->
 service {'neutron-metadata-agent':
